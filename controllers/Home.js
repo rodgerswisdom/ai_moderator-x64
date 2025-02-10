a2456c8f03d8f749c70459da0142f1c4a0c3bcd6
rodgerswisdom <trulyhawona@gmail.com>
2025-02-10 23:33:16 +0300

    Automated commit

---
+++ controllers/Home.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const models = require('../utils/db.js');
+
+
+static async function index(req, res){
+    message = "ai_moderator";
+    res = message;
+}
+
+export default Home;
