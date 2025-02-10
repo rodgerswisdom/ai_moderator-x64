17ea6604ad2c6e5e8dce4389c2ffde0195c57237
rodgerswisdom <trulyhawona@gmail.com>
2025-02-10 23:29:45 +0300

    Automated commit

--- routers/routes.js
+++ routers/routes.js
@@ -1,9 +1,11 @@
 const express = require('require');
 const router = require('router');
 const users = require('../controllers/Auth.js');
-const home = require('../home.js')
+const home = require('../controllers/Home.js');
 
 router.post('/login', users.login());
 router.get('/', home.index());
+
+export default routes;
 
 
