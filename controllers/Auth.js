87ba0ab5ec758bf2bca99c506e17e3241cd9e8b6
rodgerswisdom <trulyhawona@gmail.com>
2025-02-12 04:00:32 +0300

    Automated commit

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 const User = require('../utils/db');
 const jwt = require('jsonwebtoken');
-
+const express = require('express');
 /**
  *
 // Create a token
@@ -19,15 +19,32 @@ jwt.verify(token, 'secret_key', (err, decodedToken) => {
 
 class Auth{
 static async signup(req, res){
-    try {
-        const { email, password, major, group } = req.body;
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User({ email, password: hashedPassword, major, group });
-        await user.save();
-        res.status(201).json({ message: 'User registered successfully' });
-    } catch (error) {
+    
+    let user = await User.findOne({ email: req.body.email })
+    if (user) {
+        return res.status(400).send('User already exisits. Please sign in')
+    } else {
+        try {
+       // const { email, password, major, group } = req.body;
+        //const hashedPassword = await bcrypt.hash(password, 10);
+            const user = new User(
+                { 
+                    email:req.body.email, 
+                    //password: hashedPassword, 
+                    password:req.body.password,
+                    major:req.body.major, 
+                    group:req.body.group 
+                }
+            );
+            await user.save();
+            res.status(201).json({ message: 'User registered successfully' });
+        } catch (error) {
         res.status(500).json({ error: 'Registration failed' });
+        console.error(error);
+        }
 }
-}
-}
+        }
+    }
+
+
 module.exports = Auth;
