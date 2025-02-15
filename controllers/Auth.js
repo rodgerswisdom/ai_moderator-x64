90089b0e383bad62b908a97f63d89719235e3ee9
rodgerswisdom <trulyhawona@gmail.com>
2025-02-16 00:45:50 +0300

    Automated commit

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,10 +1,11 @@
 const User = require('../utils/db');
 const express = require('express');
-
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 class Auth{
 
-     checkuser({email}){
+     checkuser(email){
         return User.findOne({email});  
     }
     
@@ -12,9 +13,10 @@ class Auth{
    
         const {email, password, major, group} = req.body; 
         
+        let hashedPassword = await bcrypt.hash(password,10);
         const AuthInstance = new Auth();
 
-        let user =  await AuthInstance.checkuser({email});
+        let user =  await AuthInstance.checkuser(email);
 
         if(user){
             return res.status(400).send("User already Exists");
@@ -22,7 +24,7 @@ class Auth{
             try{
                 const user = new User({
                     email,
-                    password,
+                    password: hashedPassword,
                     major,
                     group 
                 })
@@ -39,20 +41,28 @@ class Auth{
     static async login(req, res){
         const {email, password} = req.body;
 
-        const AuthInstance = new Auth();
 
-        let user =  await AuthInstance.checkuser({email});
+        // const AuthInstance = new Auth();
+
+        let user =  await User.findOne({email});
 
         if(!user){
-            return res.status(400).send("You dont have an account. Sign up");
+            return res.status(401).send("You dont have an account. Sign up");
         } 
 
-        let checkpassword = await User.findOne({password});
-        if(checkpassword==={password}){
-            res.status(200).send("Login successful");
-        }        
-    }
+        let comparePassword = await bcrypt.compare(password, user.password);
+        if(!comparePassword){
+            res.status(401).send("Wrong Password");
+        }
 
+        const token = jwt.sign({user_id:user._id}, 'my_secret', {expiresIn:60*60});
+        
+        res.status(200).send(`Login successful, token ${token}`);
+    } catch(e){
+        res.status(401).send("Login failed");
+        console.log(e);
+    }
+        
 }
 
 
