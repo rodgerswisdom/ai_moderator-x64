4e31dc85960d3b534774a1e559148dc74b546da8
rodgerswisdom <trulyhawona@gmail.com>
2025-03-12 16:07:57 +0300

    Implement Auth views

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,15 @@
 import React from "react";
-import ChatWindow from "./components/ChatWindow";
-import ChatHistory from "./components/ChatHistory";
-import ChatInput from "./components/ChatInput";
-import BlankSpace from "./components/BlankSpace";
-import sendMessageToAI from "./services/api";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/SignUp";
 
-export default function App(){
-    const [chatResponse, setChatResponse] = useState(null);
 
-    const handleSend = async (message) =>{
-        try{
-            const res = await sendMessageToAI(message)
-            setChatResponse(res)
-        } catch(e){
-            setChatResponse(`Error to send chat ${e}`);
-        }
-    }
-
-    return(
-        <div className="flex h-screen w-screen">
-            <ChatHistory />
-            <div className="flex-1 flex flex-col">
-                <ChatWindow  chatResponse={chatResponse}/>
-                 <ChatInput onSend ={handleSend}/>
+export default function App() {
+    return (
+        <>
+            <div className="flex h-screen w-screen">
+                {/* <Login /> */}
+                <Signup />
             </div>
-            <BlankSpace />
-            
-        </div>
+        </>
     );
 }
--- frontend/src/components/ChatWindow.jsx
+++ frontend/src/components/ChatWindow.jsx
@@ -4,7 +4,7 @@ const ChatWindow = ({ chatResponse }) => {
     <div className="flex-1 flex-col overflow-y-auto p-4 bg-gray-100">
         <h1 className="text-black">Chat Window</h1> 
         <div className="bg-grey">
-          <p className="text-black">{ chatResponse }</p>
+          <p className="text-black">chatResponse </p>
         </div>
     </div>
   );
