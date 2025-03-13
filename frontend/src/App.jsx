64b74e4d98b561f270a9c318b23d3e23a8b3e8f2
rodgerswisdom <trulyhawona@gmail.com>
2025-03-13 12:10:03 +0300

    Implement Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,25 @@
 import React from "react";
-import Login from "./components/auth/Login";
-import Signup from "./components/auth/SignUp";
+import Login from "./pages/Login";
+import Signup from "./pages/SignUp";
+import Home from "./pages/Home";
+import Navbar from "./components/layout/Navbar";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom"; // FIX: Use 'react-router-dom'
 
 export default function App() {
     return (
-        <>
-            <div className="flex h-screen w-screen">
-                <div className="flex h-screen w-screen">
-                    <h1>educonnect</h1>
-                    <div>
-                        <button>Signup</button>
-                        <button>Login</button>
-                    </div>
-                </div>
-            </div>
-        </>
+        <div className="min-h-screen w-full flex flex-col">
+            {/* Navbar Always Visible */}
+            <BrowserRouter>
+                <Navbar />
+                <main className="flex-grow bg-gray-100">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<Signup />} />
+                    </Routes>
+                </main>
+            </BrowserRouter>
+        </div>
     );
 }
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div className="flex h-screen w-screen items-center justify-center">
+            <h1 className="text-4xl font-bold">educonnect</h1>
+        </div>
+    );
+}
